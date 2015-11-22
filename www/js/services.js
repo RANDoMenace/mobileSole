a9504f1b7bc4f5e0436b8d4b08fc4b039c8e9540
Bryan Garza <bgarza1016@gmail.com>
2015-11-22 14:35:34 -0800

    login with sessions. working towards authO.

---
+++ www/js/services.js
@@ -0,0 +1,7 @@
+// services.js
+
+angular.module('starter.services', ['ngResource'])
+
+.factory('Session', function($resource) {
+  return $resource('http://localhost:5000/sessions/:sessionId');
+});
